014789522b8f4477d7413bf3c59d8704cdc172c1
PavloBilenko <bilenko.pavlo@gmail.com>
2024-08-19 05:07:32 +0200

    01

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputEL = document.querySelector('#name-input');
-const outputEL = document.querySelector('#name-output');
-
-inputEL.addEventListener('input', () => {
-  const trimmedValue = inputEL.value.trim();
-  outputEL.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
-});
